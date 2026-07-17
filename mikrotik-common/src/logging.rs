7f5b24d64e12d2ed4fd0ed3a29271ebce23eaef3
Luis Schwab <luisschwab@protonmail.com>
2026-07-17 17:11:50 -0300

    chore(common): fix `tracing` macros

--- mikrotik-common/src/logging.rs
+++ mikrotik-common/src/logging.rs
@@ -3,7 +3,7 @@
 #[cfg(feature = "tracing-subscriber")]
 use tracing_subscriber::EnvFilter;
 
-/// Initialize `tracing-subscriber` from `RUST_LOG`, defaulting to `info`.
+/// Initialize a `tracing-subscriber` from `RUST_LOG`, defaulting to `info`.
 ///
 /// This is intended for examples and small CLIs that only need a stdout/stderr
 /// formatter. Larger binaries should keep using their own logging setup when
@@ -29,39 +29,39 @@ macro_rules! impl_command_display {
 /// Emit one `trace` event prefixed with a label.
 #[macro_export]
 macro_rules! trace_with_label {
-    ($label:expr, $($argument:tt)*) => {
-        ::tracing::trace!("{}: {}", $label, format_args!($($argument)*));
-    };
+    ($label:expr, $($argument:tt)*) => {{
+        ::tracing::trace!("{}: {}", $label, format_args!($($argument)*))
+    }};
 }
 
 /// Emit one `debug` event prefixed with a label.
 #[macro_export]
 macro_rules! debug_with_label {
-    ($label:expr, $($argument:tt)*) => {
-        ::tracing::debug!("{}: {}", $label, format_args!($($argument)*));
-    };
+    ($label:expr, $($argument:tt)*) => {{
+        ::tracing::debug!("{}: {}", $label, format_args!($($argument)*))
+    }};
 }
 
 /// Emit one `info` event prefixed with a label.
 #[macro_export]
 macro_rules! info_with_label {
-    ($label:expr, $($argument:tt)*) => {
-        ::tracing::info!("{}: {}", $label, format_args!($($argument)*));
-    };
+    ($label:expr, $($argument:tt)*) => {{
+        ::tracing::info!("{}: {}", $label, format_args!($($argument)*))
+    }};
 }
 
 /// Emit one `warning` event prefixed with a label.
 #[macro_export]
 macro_rules! warn_with_label {
-    ($label:expr, $($argument:tt)*) => {
-        ::tracing::warn!("{}: {}", $label, format_args!($($argument)*));
-    };
+    ($label:expr, $($argument:tt)*) => {{
+        ::tracing::warn!("{}: {}", $label, format_args!($($argument)*))
+    }};
 }
 
 /// Emit one `error` event prefixed with a label.
 #[macro_export]
 macro_rules! error_with_label {
-    ($label:expr, $($argument:tt)*) => {
-        ::tracing::error!("{}: {}", $label, format_args!($($argument)*));
-    };
+    ($label:expr, $($argument:tt)*) => {{
+        ::tracing::error!("{}: {}", $label, format_args!($($argument)*))
+    }};
 }
